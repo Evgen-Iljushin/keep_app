3d9827089f42a33a894f41252ea9e526f9667a8f
Iljushin Evgen <iljushinevgeniy@gmail.com>
2020-09-13 14:03:03 +0300

    update config

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ module.exports = app
 
 // Start standalone server if directly running
 if (require.main === module) {
-  const port = process.env.PORT || 3001
+  const port = 3030// process.env.PORT || 3001
   app.listen(port, () => {
     console.log(`API server listening on port ${port}`)
   })
